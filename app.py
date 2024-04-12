92fd0402d7a6df75dad4d7816b8978535f653926
YASH GOSAVI <yashgosavi@YASHs-MacBook-Air.local>
2024-04-12 10:07:08 +0530

    a

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import nltk
@@ -13,7 +13,12 @@ class SentimentAnalysis(Resource):
     def post(self):
         reviews_data = request.get_json()
 
-        grouped_reviews = {}
+        grouped_reviews = {
+            "Positive": [],
+            "Negative": [],
+            "Neutral": [],
+            "Artificial": []
+        }
         total_rating = 0
         rating_counts = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
 
@@ -33,9 +38,14 @@ class SentimentAnalysis(Resource):
             else:
                 group, buy = 'Neutral', False
 
-            if group not in grouped_reviews:
-                grouped_reviews[group] = []
-            grouped_reviews[group].append({'text': text, 'sentiment': group, 'buy': buy, 'rating': rating, 'date': date})
+            grouped_reviews[group].append({
+                'user': review.get('user', ''),
+                'text': text,
+                'sentiment': group,
+                'buy': buy,
+                'rating': rating,
+                'date': date
+            })
 
             total_rating += rating
             rating_counts[rating] += 1
@@ -44,7 +54,7 @@ class SentimentAnalysis(Resource):
         positive_percentage = len(positive_reviews) / len(reviews_data) * 100
 
         average_rating = total_rating / len(reviews_data) if len(reviews_data) > 0 else 0
-        rating_percentages = {rating: count / sum(rating_counts.values()) * 100 for rating, count in rating_counts.items()}
+        rating_percentages = {str(rating): count / sum(rating_counts.values()) * 100 for rating, count in rating_counts.items()}
 
         overall_recommendation = (
             'Based on the overwhelmingly positive reviews, I highly recommend purchasing this product.'
@@ -52,13 +62,15 @@ class SentimentAnalysis(Resource):
             else 'While there are some negative reviews, I\'d suggest taking a closer look at the overall feedback. If the product meets your specific needs, it could still be a worthwhile purchase despite a few negative reviews.'
         )
 
-        return {
-            'grouped_reviews': grouped_reviews,
-            'positive_percentage': positive_percentage,
-            'average_rating': average_rating,
-            'rating_percentages': rating_percentages,
-            'overall_recommendation': overall_recommendation
+        output = {
+            "grouped_reviews": grouped_reviews,
+            "positive_percentage": positive_percentage,
+            "average_rating": average_rating,
+            "rating_percentages": rating_percentages,
+            "overall_recommendation": overall_recommendation
         }
+
+        return jsonify(output)
 
 @app.route('/')
 def index():
